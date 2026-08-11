c96d122c9818a7c138595ce2b3192fc60173161e
Fatma Hassan <fatima663562@gmail.com>
2026-08-11 16:59:01 +0300

    addDBContext

---
+++ AlSaad.Persistence/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlSaad.Persistence.Context
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) :base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
